181c95b0741be1d824bf9e2d2fca8efa4c0b2d1a
m-kyuu <37369477+m-kyuu@users.noreply.github.com>
2018-07-30 15:39:31 +0900

    update scripts

--- lyric.py
+++ lyric.py
@@ -4,8 +4,13 @@ from urllib.parse import urljoin
 from tqdm import tqdm
 from time import sleep
 import MeCab
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import logging
 
 _BASE_URL = 'http://j-lyric.net'
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 def get_lyric(url):
@@ -18,7 +23,7 @@ def get_lyric(url):
 
 
 def get_link():
-    r = requests.get('http://j-lyric.net/artist/a000eac/')
+    r = requests.get('http://j-lyric.net/artist/a001c7a/')
     soup = BeautifulSoup(r.text, 'html.parser')
     url = []
     for song in soup.find_all('p', 'ttl'):
@@ -35,25 +40,49 @@ def tokenizer(text):
         feature = feature.split(',')
         speech = feature[0]
         base_form = feature[6]
-        if speech in ['名詞', '形容詞']:
+        if speech in ['名詞', '形容詞', '動詞']:
             tokens.append(surface if base_form == '*' else base_form)
     return ' '.join(tokens)
 
 
+def create_cloud(text):
+    stop_words = [
+        'ん', 'よう', '(', ')', 'ない', 'てる', 'いる', 'なる', 'れる', 'する', 'ある',
+        'こと', 'そう', 'せる', 'した', 'いい', 'くれる', 'られる'
+    ]
+    fpath= 'C:\\Windows\\Fonts\\HGRSGU.TTC'
+    wordcloud = WordCloud(font_path=fpath, background_color="white", width=900, height=500, colormap="Pastel2", stopwords=set(stop_words))
+    wordcloud.generate(text)
+    wordcloud.to_file('wordcloud/mc_pastel.png')
+    plt.figure()
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+
+
 if __name__ == '__main__':
-    # urls = get_link()
-    # lyrics = []
-    # for url in tqdm(urls):
-    #     lyrics.append(get_lyric(url))
-    #     sleep(2)
-    # lyrics = ' '.join(lyrics)
-    # with open('arashi_lyric.txt', 'w', encoding='utf-8') as f:
-    #     f.write(lyrics)
-    with open('arashi_lyric.txt', 'r', encoding='utf-8') as f:
+    urls = get_link()
+    lyrics = []
+    logging.info('Started crawling.')
+    for url in tqdm(urls):
+        lyrics.append(get_lyric(url))
+        sleep(2)
+    lyrics = ' '.join(lyrics)
+    with open('mc_lyric.txt', 'w', encoding='utf-8') as f:
+        f.write(lyrics)
+    logging.info('Saved lyrics.')
+
+    with open('mc_lyric.txt', 'r', encoding='utf-8') as f:
         data = f.read()
     data = data.split()
+    logging.info('Started tokenizing.')
     tokens = [tokenizer(text) for text in data]
     tokens = ' '.join([word.strip() for word in tokens])
-    with open('lyric_tokens.txt', 'w', encoding='utf-8') as f:
+    with open('mc_tokens.txt', 'w', encoding='utf-8') as f:
         f.write(tokens)
+    logging.info('Saved tokens.')
 
+    with open('mc_tokens.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+    create_cloud(text)
+    logging.info('Created wordcloud.')
